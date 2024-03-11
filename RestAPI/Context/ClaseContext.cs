f48e5da57d21a766c87da17622a40b223cf54884
vixxtory <victoria.xc7@gmail.com>
2024-03-11 13:31:20 -0300

    primer apirest en funcionamiento

---
+++ RestAPI/Context/ClaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestAPI.Models;
+
+namespace RestAPI.Context
+{
+    public class ClaseContext:DbContext
+    {
+        public ClaseContext(DbContextOptions<ClaseContext> options): base(options)
+        {
+        }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ RestAPI/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace RestAPI.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public required string Name { get; set; }
+        public required int Age { get; set; }
+    }
+}
